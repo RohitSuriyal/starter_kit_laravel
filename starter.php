d6c7f9ef97737e93ca3a5795649dc26a07fcbf94
RohitSuriyal <165562285+RohitSuriyal@users.noreply.github.com>
2025-02-20 11:01:38 +0530

    jquery added
    This is the part where i added the jquery

--- starter.php
+++ starter.php
@@ -32,7 +32,7 @@
             }
         });
     </script>
-
+ <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
